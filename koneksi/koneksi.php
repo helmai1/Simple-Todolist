b36b3fcfc2f59fa20a254a241bdea2f92a11bf90
Agus Prasetyo <agusprasetyo1889@gmail.com>
2019-09-04 11:40:13 +0700

    edit koneksi.php

--- koneksi/koneksi.php
+++ koneksi/koneksi.php
@@ -1,7 +1,7 @@
 <?php
    $hostname = "localhost";
    $username = "root";
-   $password = "gokpras123";
+   $password = "";
    $db_name = "todolist-example";
 
    $koneksi = mysqli_connect($hostname, $username, $password, $db_name) or trigger_error(mysqli_error($koneksi), E_USER_NOTICE);
